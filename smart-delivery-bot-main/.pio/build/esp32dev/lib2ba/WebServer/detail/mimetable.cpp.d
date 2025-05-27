24e76adf0549f78b4f7ec8c2d39a38152544e06a
oxxultus <a38027928@gmail.com>
2025-05-27 12:46:03 +0900

    end point

--- smart-delivery-bot-main/.pio/build/esp32dev/lib2ba/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/lib2ba/WebServer/detail/mimetable.cpp.o: \
- /Users/oxxultus/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- /Users/oxxultus/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- /Users/oxxultus/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
